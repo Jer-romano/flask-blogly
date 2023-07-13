d9c059bce1b28bee85a0cdf084215f3fdcede571
Jeremy Romano <jar315@pitt.edu>
2023-07-13 00:56:05 -0400

    Added posts feature. Full CRUD

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 from flask import Flask, render_template, redirect, request
 from sqlalchemy import text 
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -69,6 +69,44 @@ def delete_user(user_id):
     db.session.commit()
     return redirect('/users')
 
+@app.route('/users/<int:user_id>/posts/new')
+def show_create_post_page(user_id):
+    user = User.query.get(user_id) #Do I need to keep making this query?
+    return render_template('create_post.html', user=user)
 
 
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def create_post(user_id):
+    title = request.form['title']
+    content = request.form['content']
+    new_post = Post(title=title, content=content, author_id=user_id)
 
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f'/users/{user_id}')
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get(post_id)
+    return render_template('post_detail.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["GET"])
+def show_edit_post_form(post_id):
+    post = Post.query.get(post_id)
+    return render_template('edit_post.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def edit_post(post_id):
+    post = Post.query.get(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+    db.session.add(post)
+    db.session.commit()
+    return redirect(f'/posts/{post_id}')
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def delete_post(post_id):
+    post = Post.query.get(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(f'/users/{post.author_id}')
--- models.py
+++ models.py
@@ -1,11 +1,17 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
 def connect_db(app):
     db.app = app
     db.init_app(app)
+
+def getCurrentDateTime():
+    now = datetime.now()
+    dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+    return dt_string
 
 #Models go below
 
@@ -27,3 +33,25 @@ class User(db.Model):
     def get_by_last_name(cls, last_name):
         '''Get all users with a particular last name'''
         return cls.query.filter_by(last_name=last_name).all()
+    
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.String(50),
+                        nullable=False)
+    content = db.Column(db.Text,
+                        nullable=False)
+    created_at = db.Column(db.DateTime,
+                            default=getCurrentDateTime())
+    author_id = db.Column(db.Integer,
+                          db.ForeignKey('users.id'))
+    author = db.relationship('User', backref="posts")
+
+
+ 
